f8b5791e0380fcdc26434ce8e9263ff0d10204d7
Akshat Manohar <51690209+akshatmanohar21@users.noreply.github.com>
2024-04-21 12:20:50 +0530

    app.py

--- hackbmu_ml_model/crop recommendation/app.py
+++ hackbmu_ml_model/crop recommendation/app.py
@@ -4,22 +4,19 @@ import numpy as np
 import lightgbm as lgb
 import pickle
 
-# Load the trained LightGBM model
 with open('crop_recommendation_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Function to predict crop based on input features
 def predict_crop(N, P, K, temperature, humidity, ph, rainfall):
     input_data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
     prediction = model.predict(input_data)
     return prediction[0]
 
-# Streamlit app
 def main():
     # Title
     st.title("Crop Recommendation System")
 
-    # Sidebar with input fields
+
     st.sidebar.header("Input Parameters")
 
     N = st.sidebar.slider("Nitrogen (N) ratio in soil (kg/ha)", 0.0, 200.0, 100.0)
@@ -30,7 +27,7 @@ def main():
     ph = st.sidebar.slider("pH value of the soil", 0.0, 14.0, 7.0)
     rainfall = st.sidebar.slider("Rainfall (mm)", 0.0, 1000.0, 500.0)
 
-    # Predict crop based on input features
+
     prediction = predict_crop(N, P, K, temperature, humidity, ph, rainfall)
     crop_list = ['apple', 'banana', 'blackgram', 'chickpea', 'coconut', 'coffee', 'cotton',
                  'grapes', 'jute', 'kidneybeans', 'lentil', 'maize', 'mango', 'mothbeans',
@@ -38,7 +35,7 @@ def main():
                  'rice', 'watermelon']
     predicted_crop = crop_list[prediction]
 
-    # Display prediction
+
     st.subheader("Predicted Crop:")
     st.write(predicted_crop)
 
